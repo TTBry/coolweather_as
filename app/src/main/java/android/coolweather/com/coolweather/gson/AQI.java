e80c96e95d1034fc3bad1b5f2591571e43f58238
TTBry <289735174@qq.com>
2016-12-16 12:05:07 +0800

    完成天气数据显示功能

---
+++ app/src/main/java/android/coolweather/com/coolweather/gson/AQI.java
@@ -0,0 +1,14 @@
+package android.coolweather.com.coolweather.gson;
+
+/**
+ * Created by HLJKKJ on 2016/12/16.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+
+}
